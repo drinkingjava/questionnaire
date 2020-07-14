2a7bf1d5a0b7d8e70c7e8df793fb89e49b302a81
algerchen <algerchen@umantec.net>
2020-07-14 03:18:46 +0200

    Added global templates directory setting. fixed __str__ method of questioninstance. Moved question_result template to correct location and updated it. Updated questions views file to handle question attempts. Added requirements file

--- questions/models.py
+++ questions/models.py
@@ -18,7 +18,7 @@ class Question(models.Model):
     question = models.TextField(
         max_length=600, help_text='Enter Question here')
     answer = models.CharField(
-        max_length=500, help_text='Enter the answer that returns the correct result i.e a number ')
+        max_length=500, help_text='Enter the solution i.e a number ')
     category = models.ManyToManyField(
         Category, help_text='Select a category for this question')
 
@@ -52,47 +52,4 @@ class QuestionInstance(models.Model):
     )
 
     def __str__(self):
-        return self.name
-
-
-class ProgrammingQuestion(models.Model):
-    # Can extend the Question model to avoid DRY
-    name = models.CharField(
-        max_length=255, help_text='Enter the name of the question')
-    question = models.TextField(
-        max_length=600, help_text='Enter Question here')
-    # For code to work properly this should be a field that respects formatting when saved
-    answer = models.CharField(
-        max_length=500, help_text='Enter the python program that returns the correct result ')
-    category = models.ManyToManyField(
-        Category, help_text='Select a category for this question')
-
-    def __str__(self):
-        return self.name
-
-
-class ProgrammingQuestionInstance(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, help='Unique Id for this attempted question')
-    question = models.ForeignKey(
-        'Question', on_delete=models.SET_NULL, null=True)
-    user = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, blank=True)
-    attempted_at = models.DateField(null=True, blank=True)
-    attempt = models.CharField(max_length=255)
-
-    ATTEMPT_STATUS = (
-        ('p', 'Pass'),
-        ('f', 'Fail'),
-        ('n', 'No attempt')
-    )
-
-    status = models.CharField(
-        max_length=1,
-        choices=ATTEMPT_STATUS,
-        blank=True,
-        default='n',
-        help_text='Attempt Status'
-    )
-
-    def __str__(self):
-        return self.name
+        return self.question.name
--- questions/views.py
+++ questions/views.py
@@ -1,21 +1,62 @@
 from django.views import generic
-from questions.models import Question
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.forms import ValidationError
 
 from .models import Question, QuestionInstance
 from .forms import AnswerForm
 
 
+def check_answer(q_model, answer):
+    return q_model.answer == answer
+
+
 def question_detail_view(request, question_id):
     # Generate counts of some of the main objects
     context = {}
-    if request.method == 'GET':
-        question = get_object_or_404(Question, pk=question_id)
-        context['question'] = question
-        context['form'] = AnswerForm
-        return render(request, 'questions/question_detail.html', context)
+    status = 'f'
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        q = get_object_or_404(Question, pk=question_id)
+        if form.is_valid():
+            attempt = form.cleaned_data.get('attempt')
+            q_instance = QuestionInstance(
+                question=q, user=request.user, attempt=attempt)
+            if attempt == q.answer:
+                q_instance.status = 'p'
+            else:
+                q_instance.status = 'f'
+            q_instance.save()
+            context['question_instance'] = q_instance
+        return render(request, 'questions/question_result.html', context)
+
+    else:
+        context['question'] = get_object_or_404(Question, pk=question_id)
+        context['attempts'] = QuestionInstance.objects.filter(
+            user=request.user)
+        print(QuestionInstance.objects.filter(user=request.user, pk=1))
+        context['form'] = AnswerForm()
+
+    return render(request, 'questions/question_detail.html', context)
+
+    # if request.method == 'GET':
+    #     question = get_object_or_404(Question, pk=question_id)
+    #     context['question'] = question
+    #     context['form'] = AnswerForm
+    #     return render(request, 'questions/question_detail.html', context)
+    # elif request.method == 'POST':
+    #     form = request.POST['attempt']
+    #     print(request.POST)
+    #     q = Question.objects.filter(id=question_id)
+    #     # if form.is_valid():
+    #     #     cleaned_data = form.cleaned_data()
+    #     #     print(cleaned_data)
+    #     context['question'] = {
+    #         'name': 'Example Question',
+    #         'status': 'Passed or Failed'
+    #     }
+    #     return render(request, 'questions/question_result.html', context)
 
 
 def attempt_question(request, question_id):
@@ -43,10 +84,13 @@ class QuestionListView(generic.ListView):
         return super().dispatch(*args, **kwargs)
 
 
-@login_required
 class QuestionDetailView(generic.DetailView):
     model = Question
     form_class = AnswerForm
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 def attempt_question(request):
